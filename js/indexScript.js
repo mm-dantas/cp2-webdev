21a5e05bf5d9148a3de8a1a6daaff090b08eaaa6
Marco Antonio Gonçalves <marcotostines@hotmail.com>
2024-04-29 18:23:07 -0300

    arrumei o js e a main page

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,3 +1,11 @@
-function comprar(){
-    window.location.href = "./detalhesproduto.html"
+function comprar1(){
+    window.location.href = "./Freixenet Pinot Grigio.html"
+}
+
+function comprar2(){
+    window.location.href = "./Luiz Argenta Jovem Red Audrey.html"
+}
+
+function comprar3(){
+    window.location.href = "./Manus Liberum Barbera Clairet Rosé.html"
 }
